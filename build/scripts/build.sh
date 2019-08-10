f115f5ad63786d8bc8c216ba930b2b831da39c7b
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2019-08-10 15:35:28 -0400

    Continue ...

---
+++ build/scripts/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+echo "Building ..."
+echo "Docker Registry : $DOCKER_REGISTRY"
+echo "Docker Image Name : $DOCKER_FULL_IMAGE_NAME"
+
+echo "$DOCKER_CREDENTIALS_PSW" | docker login --username "$DOCKER_CREDENTIALS_USR" --password-stdin "$DOCKER_REGISTRY"
+
+docker build -t "$DOCKER_FULL_IMAGE_NAME" -f docker/production.Dockerfile .
+
+docker push "$DOCKER_FULL_IMAGE_NAME"
