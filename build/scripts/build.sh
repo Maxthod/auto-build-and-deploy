61e0fa1037d4998dee7896a5732530a073e187d5
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2019-08-11 18:21:04 -0400

    Continue ...

--- build/scripts/build.sh
+++ build/scripts/build.sh
@@ -3,12 +3,12 @@ set -e
 
 echo "Building script ..."
 echo "Docker Registry : $DOCKER_REGISTRY"
-echo "Docker Image Name : $DOCKER_FULL_IMAGE_NAME"
+echo "Docker Image Name : $DOCKER_IMAGE"
 
 echo "$DOCKER_REGISTRY_CREDENTIALS_PSW" | docker login --username "$DOCKER_REGISTRY_CREDENTIALS_USR" --password-stdin "$DOCKER_REGISTRY"
 
 cd "$GIT_CLONE_DIRECTORY"
 
-docker build -t "$DOCKER_FULL_IMAGE_NAME" -f docker/production.Dockerfile .
+docker build -t "$DOCKER_IMAGE" -f docker/production.Dockerfile .
 
-docker push "$DOCKER_FULL_IMAGE_NAME"
+docker push "$DOCKER_IMAGE"
